419e29f0cb4b560a73c3f74637743d91a86c5d88
Cdesilva2 <97003729+cdesilva2@users.noreply.github.com>
2022-10-06 14:59:00 +0000

    update

--- week04/regex.py
+++ week04/regex.py
@@ -1,5 +1,13 @@
 import re
 
+def find_name(line):
+  #pattern = r'([A-Z][a-z])'
+  pattern = r'([A-Z][a-z]+(?:\.)?(?=\s[A-Z])(?:\s[A-Z][a-z]+)+)'
+  result = re.findall(pattern,line)
+  return result 
+  
 file = open("names.txt")
 for line in file.readlines():
-  print(line)
+  result = find_name(line)
+  print(result)
+  
